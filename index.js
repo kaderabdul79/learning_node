c899f99b431fd903673afc7a5389da0c7d921337
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-17 08:30:28 +0600

    installed nodemon to run server auto and show users

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// const users = [
+//   {id:0,name: '0 abdul kader',Address : '0 dhaka'},
+//   {id:1,name: '1 abdul kader',Address : '1 dhaka'},
+//   {id:2,name: '2 abdul kader',Address : '2 dhaka'},
+//   {id:3,name: '3 abdul kader',Address : '3 dhaka'},
+//   {id:4,name: '4 abdul kader',Address : '4 dhaka'}
+// ]
+app.get('/users', (req,res) => {
+  res.send({id:4,name: '4 abdul kader',Address : '4 dhaka'})
 })
 
 app.listen(port, () => {
