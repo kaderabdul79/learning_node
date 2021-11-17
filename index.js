6d5fc4b2626ffff21f5b329fb700635033bb54e6
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-17 08:37:23 +0600

    created dynamic route,api param and access this params also show info

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// const users = [
-//   {id:0,name: '0 abdul kader',Address : '0 dhaka'},
-//   {id:1,name: '1 abdul kader',Address : '1 dhaka'},
-//   {id:2,name: '2 abdul kader',Address : '2 dhaka'},
-//   {id:3,name: '3 abdul kader',Address : '3 dhaka'},
-//   {id:4,name: '4 abdul kader',Address : '4 dhaka'}
-// ]
+const users = [
+  {id:0,name: '0 abdul kader',Address : '0 dhaka'},
+  {id:1,name: '1 abdul kader',Address : '1 dhaka'},
+  {id:2,name: '2 abdul kader',Address : '2 dhaka'},
+  {id:3,name: '3 abdul kader',Address : '3 dhaka'},
+  {id:4,name: '4 abdul kader',Address : '4 dhaka'}
+]
 app.get('/users', (req,res) => {
-  res.send({id:4,name: '4 abdul kader',Address : '4 dhaka'})
+  res.send(users)
 })
-
+app.get('/user/:id', (req,res) => {
+  // console.log(req.params.id)
+  const id = req.params.id;
+  const user = users[id];
+  console.log(res.send(user))
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
